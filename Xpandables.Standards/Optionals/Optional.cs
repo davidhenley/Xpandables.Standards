35b7c126de4e1d5862b0f830f626d110118627f6
Francescolis <francisblack.ewane@gmail.com>
2019-07-28 10:48:32 +0200

    remove optional interface implementation

--- Xpandables.Standards/Optionals/Optional.cs
+++ Xpandables.Standards/Optionals/Optional.cs
@@ -25,7 +25,7 @@ namespace System
     /// You can make unconditional calls to its contents without testing whether the content is there or not.
     /// </summary>
     /// <typeparam name="T">Type of optional result.</typeparam>
-    public partial class Optional<T> : IOptional<T>
+    public partial class Optional<T>
     {
         private readonly T[] _values;
 
