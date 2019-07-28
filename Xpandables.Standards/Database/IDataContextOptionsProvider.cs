3a12d78bb41d3acf11b0be5b4503c7da1c61c833
Francescolis <francisblack.ewane@gmail.com>
2019-07-28 14:52:09 +0200

    add TOptions provider

---
+++ Xpandables.Standards/Database/IDataContextOptionsProvider.cs
@@ -0,0 +1,33 @@
+﻿/************************************************************************************************************
+ * Copyright (C) 2019 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+************************************************************************************************************/
+
+namespace System.Data
+{
+    /// <summary>
+    /// Provides with the data context options instance.
+    /// </summary>
+    public interface IDataContextOptionsProvider<TOptions>
+        where TOptions : class
+    {
+        /// <summary>
+        /// Gets the data context options according to the environment.
+        /// </summary>
+        /// <returns>An implementation of <typeparamref name="TOptions"/>.</returns>
+        /// <exception cref="InvalidOperationException">Creating the context options failed. See inner exception.</exception>
+        TOptions GetOptions();
+    }
+}
