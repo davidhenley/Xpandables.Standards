e442a41751111be79dd2456b735eb76ba7b98589
Francescolis <francisblack.ewane@gmail.com>
2019-08-03 15:41:46 +0200

    add ICustomVersionProvider

---
+++ Xpandables.Core/Configuration/ICustomVersionProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Xpandables.Core.Configuration
+{
+    /// <summary>
+    /// Provides with method to access the current version of an application.
+    /// </summary>
+    public interface ICustomVersionProvider
+    {
+        /// <summary>
+        /// Returns the ambient version of the underlying application.
+        /// </summary>
+        /// <returns>A instance of <see cref="ApiVersion"/>.</returns>
+        ApiVersion GetVersion();
+    }
+}
