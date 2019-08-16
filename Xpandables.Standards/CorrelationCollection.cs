896b4b9a029bf0f07b6a574c86d12596e3042930
Francescolis <francisblack.ewane@gmail.com>
2019-08-16 19:58:40 +0200

    add ICorrelationCollection

---
+++ Xpandables.Standards/CorrelationCollection.cs
@@ -0,0 +1,67 @@
+﻿/************************************************************************************************************
+ * Copyright (C) 2019 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+************************************************************************************************************/
+
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace System
+{
+    /// <summary>
+    /// The correlation collection implementation.
+    /// </summary>
+    public class CorrelationCollection<TKey, TValue> : ICorrelationCollection<TKey, TValue>
+    {
+        protected AsyncLocal<ConcurrentDictionary<TKey, TValue>> Items { get; }
+
+        /// <summary>
+        /// Constructs a new instance that initializes the collection.
+        /// </summary>
+        public CorrelationCollection() => Items = new AsyncLocal<ConcurrentDictionary<TKey, TValue>>
+        {
+            Value = new ConcurrentDictionary<TKey, TValue>()
+        };
+
+        public virtual Optional<TValue> this[TKey key]
+        {
+            get
+            {
+                if (key is null)
+                    return OptionalHelpers.Empty<TValue>();
+
+                return Items.Value.TryGetValue(key, out var found) ? found : default;
+            }
+            set
+            {
+                if (key is null) return;
+                if (value is null)
+                    value = OptionalHelpers.Empty<TValue>();
+
+                Items.Value.AddOrUpdate(key, value.Cast<TValue>(), (_, __) => value.Cast<TValue>());
+            }
+        }
+
+        public virtual void AddOrUpdateValue(TKey key, TValue value) => this[key] = value;
+
+        public virtual IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => Items.Value.GetEnumerator();
+
+        public virtual Optional<TValue> GetValue(TKey key) => this[key];
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Xpandables.Standards/ICorrelationCollection.cs
@@ -0,0 +1,51 @@
+﻿/************************************************************************************************************
+ * Copyright (C) 2019 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+************************************************************************************************************/
+
+using System.Collections.Generic;
+
+namespace System
+{
+    /// <summary>
+    /// Provides a collection of objects that need to be shared across asynchronous control flows.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public interface ICorrelationCollection<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    {
+        /// <summary>
+        /// Sets the correlated object by its key. If key already exist, it'll be updated.
+        /// Gets the correlated object by its key and returns a value when found.
+        /// </summary>
+        /// <param name="key">The key of the correlated object.</param>
+        /// <returns>A object from the ambient correlation list of objects.</returns>
+        Optional<TValue> this[TKey key] { set; get; }
+
+        /// <summary>
+        /// Gets the correlated object by its key and returns a value when found.
+        /// </summary>
+        /// <param name="key">The key of the correlated object.</param>
+        /// <returns>A object from the ambient correlation list of objects.</returns>
+        Optional<TValue> GetValue(TKey key);
+
+        /// <summary>
+        /// Adds the correlated object by its key. If key already exist, it'll be updated.
+        /// </summary>
+        /// <param name="key">The key of the correlated object.</param>
+        /// <param name="value">A object to be stored to the ambient correlation list of objects.</param>
+        void AddOrUpdateValue(TKey key, TValue value);
+    }
+}
