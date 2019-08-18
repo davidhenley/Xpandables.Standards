ca257dff16e1d2b1c40386e811aaeb697b3abf80
Francescolis <francisblack.ewane@gmail.com>
2019-08-18 15:03:34 +0200

    add data context

---
+++ Xpandables.Core/Database/Asyncs/AsyncDataContext.cs
@@ -0,0 +1,128 @@
+﻿/************************************************************************************************************
+ * Copyright (C) 2019 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+************************************************************************************************************/
+
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Design.Database
+{
+    public abstract partial class DataContext
+    {
+        async Task<Optional<T>> IDataContext.FindAsync<T>(CancellationToken cancellationToken, params object[] keyValues)
+             => await FindAsync<T>(keyValues, cancellationToken).ConfigureAwait(false);
+        IAsyncEnumerable<TResult> IDataContext.GetAllAsync<T, TResult>(Func<IQueryable<T>, IQueryable<TResult>> selector)
+        {
+            if (selector is null) throw new ArgumentNullException(nameof(selector));
+            return selector(Set<T>()).AsAsyncEnumerable();
+        }
+        async Task<Optional<TResult>> IDataContext.GetFirstAsync<T, TResult>(
+           Func<IQueryable<T>, IQueryable<TResult>> selector,
+           CancellationToken cancellationToken)
+        {
+            if (selector is null) throw new ArgumentNullException(nameof(selector));
+            return await selector(Set<T>()).FirstOrDefaultAsync(cancellationToken).ConfigureAwait(false);
+        }
+        async Task<Optional<TResult>> IDataContext.GetLastAsync<T, TResult>(
+          Func<IQueryable<T>, IQueryable<TResult>> selector,
+          CancellationToken cancellationToken)
+        {
+            if (selector is null) throw new ArgumentNullException(nameof(selector));
+            return await selector(Set<T>()).LastOrDefaultAsync(cancellationToken).ConfigureAwait(false);
+        }
+        async Task IDataContext.AddAsync<T>(T entity, CancellationToken cancellationToken)
+        {
+            if (entity is null) throw new ArgumentNullException(nameof(entity));
+            await AddAsync(entity, cancellationToken).ConfigureAwait(false);
+        }
+        async Task IDataContext.AddRangeAsync<T>(IEnumerable<T> entities, CancellationToken cancellationToken)
+        {
+            if (entities is null || entities.Count() <= 0)
+                throw new ArgumentNullException(nameof(entities));
+
+            await AddRangeAsync(entities, cancellationToken).ConfigureAwait(false);
+        }
+        async Task IDataContext.DeleteAsync<T>(T entity, CancellationToken cancellationToken)
+        {
+            if (entity is null) throw new ArgumentNullException(nameof(entity));
+            Remove(entity);
+            await Task.CompletedTask.ConfigureAwait(false);
+        }
+        async Task IDataContext.DeleteRangeAsync<T>(IEnumerable<T> entities, CancellationToken cancellationToken)
+        {
+            if (entities is null || entities.Count() <= 0)
+                throw new ArgumentNullException(nameof(entities));
+
+            RemoveRange(entities);
+            await Task.CompletedTask.ConfigureAwait(false);
+        }
+        async Task IDataContext.DeleteAsync<T>(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken)
+        {
+            if (predicate is null) throw new ArgumentNullException(nameof(predicate));
+            await Set<T>()
+                .Where(predicate)
+                .ForEachAsync(entity => Remove(entity), cancellationToken)
+                .ConfigureAwait(false);
+        }
+        async Task IDataContext.UpdateAsync<T, TUpdated>(TUpdated updatedEntity, CancellationToken cancellationToken)
+        {
+            if (updatedEntity is null) throw new ArgumentNullException(nameof(updatedEntity));
+            await Set<T>().Where(entity => entity.Id == updatedEntity.Id)
+                 .ForEachAsync(entity => Entry(entity).CurrentValues.SetValues(updatedEntity), cancellationToken)
+                 .ConfigureAwait(false);
+        }
+        async Task IDataContext.UpdateRangeAsync<T, TUpdated>(
+           IReadOnlyList<TUpdated> updatedEntities,
+           CancellationToken cancellationToken)
+        {
+            if (updatedEntities is null || updatedEntities.Count() <= 0)
+                throw new ArgumentNullException(nameof(updatedEntities));
+
+            foreach (var updatedEntity in updatedEntities)
+                await Set<T>().Where(entity => entity.Id == updatedEntity.Id)
+                    .ForEachAsync(entity => Entry(entity).CurrentValues.SetValues(updatedEntity), cancellationToken)
+                    .ConfigureAwait(false);
+        }
+        async Task IDataContext.UpdateAsync<T, TUpdated>(
+          Expression<Func<T, bool>> predicate,
+          Func<T, TUpdated> updater,
+          CancellationToken cancellationToken)
+        {
+            if (predicate is null) throw new ArgumentNullException(nameof(predicate));
+            if (updater is null) throw new ArgumentNullException(nameof(updater));
+
+            await Set<T>().Where(predicate)
+                 .ForEachAsync(entity => Entry(entity).CurrentValues.SetValues(updater(entity)), cancellationToken)
+                 .ConfigureAwait(false);
+        }
+
+        async Task IDataContext.PersistAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                await SaveChangesAsync(true, cancellationToken).ConfigureAwait(false);
+            }
+            catch (Exception exception) when (exception is DbUpdateException)
+            {
+                throw new InvalidOperationException("Persistence operation failed. See inner exception", exception);
+            }
+        }
+    }
+}
---
+++ Xpandables.Core/Database/DataContextHelpers.cs
@@ -0,0 +1,77 @@
+﻿/************************************************************************************************************
+ * Copyright (C) 2018 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+************************************************************************************************************/
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Linq;
+using System.Reflection;
+
+namespace System.Design.Database
+{
+#pragma warning disable HAA0401 // Possible allocation of reference type enumerator
+    /// <summary>
+    /// Extends the <see cref="ModelBuilder"/> class to apply conversion to <see cref="EnumerationType"/> types.
+    /// </summary>
+    /// <remarks>From https://github.com/aspnet/EntityFrameworkCore/issues/10784</remarks>
+    public static class ModelBuilderHelpers
+    {
+        /// <summary>
+        /// Specifies the converter to be used for the generic property type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The generic target <typeparamref name="T"/> to act on.</typeparam>
+        /// <param name="this">The current model builder instance.</param>
+        /// <param name="valueConverter">The converter to be applied.</param>
+        /// <returns>The model builder with converter application.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="this"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">The <paramref name="valueConverter"/> is null.</exception>
+        public static ModelBuilder UseEnumerationValueConverterForType<T>(
+            this ModelBuilder @this,
+            ValueConverter<T, string> valueConverter)
+            where T : EnumerationType
+            => @this.UseEnumerationValueConverterForType(typeof(T), valueConverter);
+
+        /// <summary>
+        /// Specifies the converter to be used for the property type (<see cref="EnumerationType"/>).
+        /// </summary>
+        /// <param name="this">The current model builder instance.</param>
+        /// <param name="type">The target type to act on.</param>
+        /// <param name="valueConverter">The converter to be applied.</param>
+        /// <returns>The model builder with converter application.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="this"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">The <paramref name="type"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">The <paramref name="valueConverter"/> is null.</exception>
+        public static ModelBuilder UseEnumerationValueConverterForType(
+            this ModelBuilder @this,
+            Type type,
+            ValueConverter valueConverter)
+        {
+            var isTypeEnumerationFunc = new Func<IMutableEntityType, bool>(IsTypeEnumeration);
+            var isPropertyTypeFunc = new Func<PropertyInfo, bool>(IsPropertyType);
+
+            foreach (var entityType in @this.Model.GetEntityTypes().Where(isTypeEnumerationFunc))
+                foreach (var property in entityType.ClrType.GetProperties().Where(isPropertyTypeFunc))
+                    @this.Entity(entityType.Name).Property(property.Name).HasConversion(valueConverter);
+
+            return @this;
+
+            static bool IsTypeEnumeration(IMutableEntityType entityType)
+                => !entityType.ClrType.IsSubclassOf(typeof(EnumerationType));
+            bool IsPropertyType(PropertyInfo propertyInfo) => propertyInfo.PropertyType == type;
+        }
+    }
+}
