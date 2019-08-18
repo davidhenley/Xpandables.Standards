c320e5f7769a4dc21d0694506c354128a9526291
Francescolis <francisblack.ewane@gmail.com>
2019-08-18 10:32:34 +0200

    add async mediator

--- Xpandables.Standards/Mediators/Asyncs/AsyncCommandQueryHandler.cs
+++ Xpandables.Standards/Mediators/Asyncs/AsyncCommandQueryHandler.cs
@@ -18,71 +18,81 @@
 using System.ComponentModel.DataAnnotations;
 using System.Design.Command;
 using System.Design.Query;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace System.Design.Mediator
 {
     /// <summary>
-    /// The default implementation for <see cref="ICommandQueryHandler"/>.
-    /// Implements methods to execute the <see cref="IQueryHandler{TQuery, TResult}"/> and
-    /// <see cref="ICommandHandler{TCommand}"/> process dynamically.
+    /// The default implementation for <see cref="IAsyncCommandQueryHandler"/>.
+    /// Implements methods to execute the <see cref="IAsyncQueryHandler{TQuery, TResult}"/> and
+    /// <see cref="IAsyncCommandHandler{TCommand}"/> process dynamically.
     /// This class can not be inherited.
     /// </summary>
-    public sealed class CommandQueryHandler : ICommandQueryHandler
+    public sealed class AsyncCommandQueryHandler : IAsyncCommandQueryHandler
     {
         private readonly IServiceProvider _serviceProvider;
 
-        public CommandQueryHandler(IServiceProvider serviceProvider)
+        public AsyncCommandQueryHandler(IServiceProvider serviceProvider)
             => _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
 
-        public void HandleCommand<TCommand>(TCommand command)
+        public async Task HandleCommandAsync<TCommand>(TCommand command, CancellationToken cancellationToken = default)
             where TCommand : class, ICommand
         {
             try
             {
                 if (command is null) throw new ArgumentNullException(nameof(command));
 
-                _serviceProvider.GetService<ICommandHandler<TCommand>>()
+                await _serviceProvider.GetService<IAsyncCommandHandler<TCommand>>()
                    .Reduce(() => throw new NotImplementedException(
                        ErrorMessageResources.CommandQueryHandlerMissingImplementation
                         .StringFormat(nameof(TCommand))))
-                   .Map(handler => handler.Handle(command));
+                   .MapAsync((handler, cancel) => handler.HandleAsync(command, cancel), cancellationToken)
+                   .ConfigureAwait(false);
             }
             catch (Exception exception) when (!(exception is ArgumentException)
                                             && !(exception is ValidationException)
                                             && !(exception is NotImplementedException)
+                                            && !(exception is OperationCanceledException)
                                             && !(exception is InvalidOperationException))
             {
                 throw new InvalidOperationException(
-                    ErrorMessageResources.CommandQueryHandlerFailed.StringFormat(nameof(HandleCommand)),
+                    ErrorMessageResources.CommandQueryHandlerFailed.StringFormat(nameof(HandleCommandAsync)),
                     exception);
             }
         }
 
-        public TResult HandleQueryResult<TQuery, TResult>(TQuery query)
+        public async Task<TResult> HandleQueryResultAsync<TQuery, TResult>(
+            TQuery query,
+            CancellationToken cancellationToken = default)
             where TQuery : class, IQuery<TResult>
         {
             try
             {
                 if (query is null) throw new ArgumentNullException(nameof(query));
 
-                return _serviceProvider.GetService<IQueryHandler<TQuery, TResult>>()
+                return await _serviceProvider.GetService<IAsyncQueryHandler<TQuery, TResult>>()
                     .Reduce(() => throw new NotImplementedException(
                         ErrorMessageResources.CommandQueryHandlerMissingImplementation
                             .StringFormat(nameof(TQuery))))
-                    .Map(handler => handler.Handle(query));
+                    .MapAsync((handler, cancel) => handler.HandleAsync(query, cancel), cancellationToken)
+                    .ConfigureAwait(false);
             }
             catch (Exception exception) when (!(exception is ArgumentException)
                                             && !(exception is ValidationException)
                                             && !(exception is NotImplementedException)
+                                            && !(exception is OperationCanceledException)
                                             && !(exception is InvalidOperationException))
             {
                 throw new InvalidOperationException(
-                    ErrorMessageResources.CommandQueryHandlerFailed.StringFormat(nameof(HandleQueryResult)),
+                    ErrorMessageResources.CommandQueryHandlerFailed.StringFormat(nameof(HandleQueryResultAsync)),
                     exception);
             }
         }
 
-        public TResult HandleResult<TResult>(IQuery<TResult> query)
+        public async Task<TResult> HandleResultAsync<TResult>(
+            IQuery<TResult> query,
+            CancellationToken cancellationToken = default)
         {
             try
             {
@@ -91,19 +101,21 @@ namespace System.Design.Mediator
                 var wrapperType = typeof(QueryHandlerWrapper<,>)
                     .MakeGenericType(new Type[] { query.GetType(), typeof(TResult) });
 
-                return _serviceProvider.GetService<IQueryHandlerWrapper<TResult>>(wrapperType)
+                return await _serviceProvider.GetService<IAsyncQueryHandlerWrapper<TResult>>(wrapperType)
                     .Reduce(() => throw new NotImplementedException(
                         ErrorMessageResources.CommandQueryHandlerMissingImplementation
                             .StringFormat(query.GetType().Name)))
-                    .Map(handler => handler.Handle(query));
+                    .MapAsync((handler, cancel) => handler.HandleAsync(query, cancel), cancellationToken)
+                    .ConfigureAwait(false);
             }
             catch (Exception exception) when (!(exception is ArgumentException)
                                             && !(exception is ValidationException)
                                             && !(exception is NotImplementedException)
+                                            && !(exception is OperationCanceledException)
                                             && !(exception is InvalidOperationException))
             {
                 throw new InvalidOperationException(
-                    ErrorMessageResources.CommandQueryHandlerFailed.StringFormat(nameof(HandleResult)),
+                    ErrorMessageResources.CommandQueryHandlerFailed.StringFormat(nameof(HandleResultAsync)),
                     exception);
             }
         }
