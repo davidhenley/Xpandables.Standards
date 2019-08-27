08f3f15f117f528b7fdb60bda66746132b345d53
Francescolis <francisblack.ewane@gmail.com>
2019-08-27 14:02:29 +0200

    add IInstanceCreator using expression.

---
+++ Xpandables.Tests/InstanceCreatorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Design.Command;
+using System.Text;
+using Xunit;
+
+namespace Xpandables.Tests
+{
+    public class InstanceCreatorTests
+    {
+        [Fact]
+        public void CreateInstance()
+        {
+            var type = typeof(CommandHandlerBuilder<>).MakeGenericType(typeof(CmdTest));
+            var creator = new InstanceCreator();
+            var instance = creator.Create(type, (Action<CmdTest>)(cmd => { }));
+            instance.Map(value => Assert.Equal(typeof(CommandHandlerBuilder<CmdTest>), value.GetType()));
+        }
+
+        class CmdTest : ICommand { }
+    }
+}
