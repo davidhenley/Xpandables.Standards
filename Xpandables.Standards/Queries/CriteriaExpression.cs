628ecff7381956387356b910be8f6db35dc13af7
Francescolis <francisblack.ewane@gmail.com>
2019-08-30 10:25:06 +0200

    add ICriteriaExpression

---
+++ Xpandables.Standards/Queries/CriteriaExpression.cs
@@ -0,0 +1,47 @@
+﻿/************************************************************************************************************
+ * Copyright (C) 2019 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+************************************************************************************************************/
+
+using System.Design.Linq;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+
+namespace System.Design.Query
+{
+    /// <summary>
+    /// This class is a helper that provides a default implementation for <see cref="ICriteriaExpression{TSource}"/>.
+    /// </summary>
+    /// <typeparam name="TSource">The data source type.</typeparam>
+    public class CriteriaExpression<TSource> : ICriteriaExpression<TSource>
+        where TSource : class
+    {
+        public Expression<Func<TSource, bool>> Expression { get; protected set; } = PredicateBuilder.New<TSource>();
+
+        [SuppressMessage(
+            "Usage", "CA2225:Les surcharges d'opérateur offrent d'autres méthodes nommées", Justification = "<En attente>")]
+        [SuppressMessage(
+            "Design", "CA1062:Valider les arguments de méthodes publiques", Justification = "<En attente>")]
+        public static implicit operator Expression<Func<TSource, bool>>([NotNull] CriteriaExpression<TSource> criteria)
+             => criteria.Expression;
+
+        [SuppressMessage(
+            "Usage", "CA2225:Les surcharges d'opérateur offrent d'autres méthodes nommées", Justification = "<En attente>")]
+        [SuppressMessage(
+            "Design", "CA1062:Valider les arguments de méthodes publiques", Justification = "<En attente>")]
+        public static implicit operator Func<TSource, bool>([NotNull] CriteriaExpression<TSource> criteria)
+            => criteria!.Expression.Compile();
+    }
+}
---
+++ Xpandables.Standards/Queries/ICriteriaExpression.cs
@@ -0,0 +1,36 @@
+﻿/************************************************************************************************************
+ * Copyright (C) 2019 Francis-Black EWANE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+************************************************************************************************************/
+
+using System.Linq.Expressions;
+
+namespace System.Design.Query
+{
+    /// <summary>
+    /// Defines a methods that returns an <see cref="Expression{TDelegate}"/> that can be used to query
+    /// the <typeparamref name="TSource"/> instance.
+    /// This can significantly improve the use of the command/query pattern, allowing class conversion to an expression tree.
+    /// </summary>
+    /// <typeparam name="TSource">The data type to apply expression to.</typeparam>
+    public interface ICriteriaExpression<TSource>
+        where TSource : class
+    {
+        /// <summary>
+        /// Gets the expression tree for the underlying instance.
+        /// </summary>
+        Expression<Func<TSource, bool>> Expression { get; }
+    }
+}
