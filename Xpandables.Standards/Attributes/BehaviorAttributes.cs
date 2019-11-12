baee9990933436561329f46da18c461dce548769
Francis-Black EWANE <fewane@efb-entreprises.com>
2019-11-12 20:42:02 +0100

    fix comments

--- Xpandables.Standards/Attributes/BehaviorAttributes.cs
+++ Xpandables.Standards/Attributes/BehaviorAttributes.cs
@@ -59,7 +59,7 @@ namespace System
     /// A marker interface that allows the class implementation to add event after control flow. In the class handling the query or command, you should reference
     /// the <see cref="ICorrelationTask"/> and set the <see cref="ICorrelationTask.PostEvent"/> and/or <see cref="ICorrelationTask.RollbackEvent"/>.
     /// Note that <see cref="ICorrelationTask.PostEvent"/> will be executed at the end of the control only if there is no exception, giving you access to all data
-    /// still alive on the control flow and the <see cref="ICorrelationTask.PostEvent"/> will only be executed when exception. The exception in that case in
+    /// still alive on the control flow and the <see cref="ICorrelationTask.RollbackEvent"/> will only be executed when exception. The exception in that case in
     /// accessible through the <see cref="ICorrelationTask.RollbackEvent"/>. You need to register the expected behavior using the appropriate extension method :
     /// <see langword="AddXCorrelationBehavior"/>.
     /// <see cref="IQuery{TResult}"/> class implementation will be decorated with <see cref="QueryCorrelationBehavior{TQuery, TResult}"/> while
